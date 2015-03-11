a9f3f46c41c89fb6b7d627f7c9423f52636e949d
Monji Dolon <mdolon@gmail.com>
2015-03-11 10:40:11 -0400

    Update verbage

--- js/massive.js
+++ js/massive.js
@@ -1,8 +1,9 @@
 $(function(){
   $(".type").typed({
-    strings: ["a group of web experts.", "a consultancy for your online needs.", "building products that will change the world.", "ironically a very small corporation.", "your next business partner."],
+    strings: ["a consultancy for your technology needs.", "a group of digital experts.", "building products that will change the world.", "an ironically small corporation.", "your next business partner."],
     typeSpeed: 30,
     backDelay: 5000,
-    loop: false
+    loop: false,
+    contentType: 'text'
   });
 });
