71518199de4afeb56e849f09b8d0d42c7aee71bf
Monji Dolon <mdolon@gmail.com>
2015-03-11 10:35:43 -0400

    Update to typewriter

---
+++ js/massive.js
@@ -0,0 +1,8 @@
+$(function(){
+  $(".type").typed({
+    strings: ["a group of web experts.", "a consultancy for your online needs.", "building products that will change the world.", "ironically a very small corporation.", "your next business partner."],
+    typeSpeed: 30,
+    backDelay: 5000,
+    loop: false
+  });
+});
